f3b40aebc65372bc2f2d7051eac9c42638c733c2
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-06-09 23:35:30 +0200

    Finished implementing chat messaging including support for multiple rooms

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,26 @@
 const chatForm = document.querySelector('#chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.querySelector('#room-name');
+const userList = document.querySelector('#users');
+
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
 
 const socket = io();
 
-socket.on('message', (message) => console.log(message));
+socket.emit('joinRoom', { username, room });
+
+socket.on('message', (message) => {
+  outputMessage(message);
+
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
 
 chatForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -14,3 +32,21 @@ chatForm.addEventListener('submit', (e) => {
   e.target.elements.msg.value = '';
   e.target.elements.msg.focus();
 });
+
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+   <p class="text">${message.text}</p>`;
+  document.querySelector('.chat-messages').appendChild(div);
+}
+
+function outputUsers(users) {
+  userList.innerHTML = `
+    ${users.map((user) => `<li>${user.username}</li>`).join('')}
+  `;
+}
+
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const path = require('path');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const {
+  getCurrentUser,
+  userJoin,
+  getRoomUsers,
+  userLeave,
+} = require('./utils/users');
 
 const PORT = process.env.PORT || 3000;
+const botName = 'DevMeetup Chat Bot';
 
 const app = express();
 
@@ -15,15 +23,48 @@ const server = app.listen(PORT, () => {
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-  socket.emit('message', 'Welcome to DevMeetup Chat');
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
-  socket.broadcast.emit('message', 'A user has joined the chat!');
+    socket.join(user.room);
 
-  socket.on('disconnect', () => {
-    io.emit('message', 'A user has left the chat');
+    socket.emit('message', formatMessage(botName, 'Welcome to DevMeetup Chat'));
+
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat!`),
+      );
+
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
   });
 
   socket.on('chatMessage', (message) => {
-    io.emit('message', message);
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit(
+      'message',
+      formatMessage(`${user.username}`, message),
+    );
+  });
+
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`),
+      );
+
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
   });
 });
